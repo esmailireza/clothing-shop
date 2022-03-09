2c03218196bbba891b6f26a663beda8684997a13
esmailireza <esmailireza20@gmail.com>
2022-03-09 16:53:52 +0330

    create and debug slider, also add styles to slider

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,9 +1,129 @@
 import Layout from "../layout/layout";
-
+import slider1 from "../assets/images/slider img/slide-01.jpg";
+import slider2 from "../assets/images/slider img/slide-02.jpg";
+import slider3 from "../assets/images/slider img/slide-03.jpg";
+import "./slider.css";
 const HomePage = () => {
   return (
     <Layout>
-      <p>this is home page</p>
+      {/* start slider */}
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-12 ">
+            <div
+              id="carouselExampleCaptions"
+              class="carousel slide"
+              data-ride="carousel"
+            >
+              <ol class="carousel-indicators">
+                <li
+                  data-target="#carouselExampleCaptions"
+                  data-slide-to="0"
+                  class="active"
+                ></li>
+                <li
+                  data-target="#carouselExampleCaptions"
+                  data-slide-to="1"
+                ></li>
+                <li
+                  data-target="#carouselExampleCaptions"
+                  data-slide-to="2"
+                ></li>
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <a href="#">
+                    <img src={slider1} class="d-block w-100" alt="..." />
+                  </a>
+                  <div class="carousel-caption d-none d-md-block carousel-caption-custome text-dark">
+                    <h2>First slide label</h2>
+                    <p>
+                      Some representative placeholder content for the first
+                      slide.
+                    </p>
+                    <button
+                      type="button"
+                      class="btn btn-primary btn-lg rounded-pill px-4 py-2"
+                    >
+                      SHOP NOW
+                    </button>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <a href="#">
+                    <img src={slider2} class="d-block w-100" alt="..." />
+                  </a>
+                  <div class="carousel-caption d-none d-md-block carousel-caption-custome text-dark">
+                    <h2>Second slide label</h2>
+                    <p>
+                      Some representative placeholder content for the second
+                      slide.
+                    </p>
+                    <button
+                      type="button"
+                      class="btn btn-primary btn-lg rounded-pill px-4 py-2"
+                    >
+                      SHOP NOW
+                    </button>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <a href="#">
+                    <img src={slider3} class="d-block w-100" alt="..." />
+                  </a>
+                  <div class="carousel-caption d-none d-md-block carousel-caption-custome text-dark">
+                    <h2>Third slide label</h2>
+                    <p>
+                      Some representative placeholder content for the third
+                      slide.
+                    </p>
+                    <button
+                      type="button"
+                      class="btn btn-primary btn-lg rounded-pill px-4 py-2"
+                    >
+                      SHOP NOW
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <button
+                class="carousel-control-prev bg-transparent border-0"
+                type="button"
+                data-target="#carouselExampleCaptions"
+                data-slide="prev"
+              >
+                <span
+                  class="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+              </button>
+              <button
+                class="carousel-control-next bg-transparent border-0"
+                type="button"
+                data-target="#carouselExampleCaptions"
+                data-slide="next"
+              >
+                <span
+                  class="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+              </button>
+            </div>
+          </div>
+          {/* <div class="col-sm-12 custom-col-md-3">
+            <div class="row">
+              <a href="#">
+                <img
+                  class="img-fluid"
+                  src="./assets/images/malte-schmidt-enGr5YbjQKQ-unsplash.jpg"
+                  alt=""
+                />
+              </a>
+            </div>
+          </div> */}
+        </div>
+      </div>
+      {/* end slider */}
     </Layout>
   );
 };
