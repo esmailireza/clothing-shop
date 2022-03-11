90711da7f3c2f92f71aaea9a57ad3a77e6eaaf9a
esmailireza <esmailireza20@gmail.com>
2022-03-11 08:55:53 +0330

    add Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import HomePage from "./pages/homePage";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Shop from "./pages/shop/shop";
 function App() {
   return (
     <Router>
       <div className="App">
-        <HomePage />
+        <Switch>
+          <Route path="/shop" component={Shop} />
+          <Route path="/" exact={true} component={HomePage} />
+        </Switch>
       </div>
     </Router>
   );
