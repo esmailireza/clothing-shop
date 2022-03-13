caf5c0ac7cb11f5dd5d55f765301f50b41f75470
esmailireza <esmailireza20@gmail.com>
2022-03-13 18:49:51 +0330

    add Remove action for delete product of cart

--- src/providers/cartProvider.js
+++ src/providers/cartProvider.js
@@ -9,7 +9,7 @@ const intialState = {
 };
 const cartReducer = (state, action) => {
   switch (action.type) {
-    case "ADD_TO_CART":
+    case "ADD_TO_CART": {
       const updatedCart = [...state.cart];
       const updatedItemIndex = updatedCart.findIndex(
         (item) => item.id === action.payload.id
@@ -21,7 +21,38 @@ const cartReducer = (state, action) => {
         updatedItem.quantity++;
         updatedCart[updatedItemIndex] = updatedItem;
       }
-      return { ...state, cart: updatedCart };
+      return {
+        ...state,
+        cart: updatedCart,
+        total: state.total + action.payload.price,
+      };
+    }
+    case "REMOVE_PRODUCT": {
+      const updatedCart = [...state.cart];
+      const updatedItemIndex = updatedCart.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      const updatedItem = { ...updatedCart[updatedItemIndex] };
+      if (updatedItem.quantity === 1) {
+        const filteredCart = updatedCart.filter(
+          (item) => item.id !== action.payload.id
+        );
+        return {
+          ...state,
+          cart: filteredCart,
+          total: state.total - action.payload.price,
+        };
+      } else {
+        updatedItem.quantity--;
+        updatedCart[updatedItemIndex] = updatedItem;
+        return {
+          ...state,
+          cart: updatedCart,
+          total: state.total - action.payload.price,
+        };
+      }
+    }
+
     default:
       return state;
   }
