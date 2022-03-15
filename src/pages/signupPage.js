2b2e82167592c4bf54b560e07f7be8bd0bcbb09c
esmailireza <esmailireza20@gmail.com>
2022-03-15 11:43:56 +0330

    create signup page

---
+++ src/pages/signupPage.js
@@ -0,0 +1,12 @@
+import SignupForm from "../components/signupForm/signupForm";
+import Layout from "../layout/layout";
+
+const SignupPage = () => {
+  return (
+    <Layout>
+      <SignupForm />
+    </Layout>
+  );
+};
+
+export default SignupPage;
