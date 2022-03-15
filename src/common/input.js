1e51df7f6cfc7e1abcfdcddc2db2729c67a8fd80
esmailireza <esmailireza20@gmail.com>
2022-03-15 11:40:27 +0330

    create input component, add styles it

---
+++ src/common/input.js
@@ -0,0 +1,20 @@
+import styles from "./input.module.css";
+const Input = ({ label, name, formik, type = "text" }) => {
+  return (
+    <div className={styles.formControl}>
+      <label htmlFor={name}>{label}</label>
+      <input
+        className={styles.inputForm}
+        id={name}
+        type={type}
+        {...formik.getFieldProps({ name })}
+        name={name}
+      />
+      {formik.errors[name] && formik.touched[name] && (
+        <div className={styles.error}>{formik.errors[name]}</div>
+      )}
+    </div>
+  );
+};
+
+export default Input;
