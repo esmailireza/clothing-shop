21d7fd756e4799d4078be57f99ee8c930464e7cd
esmailireza <esmailireza20@gmail.com>
2022-03-16 23:56:01 +0330

    create loginPage page

---
+++ src/pages/loginPage.js
@@ -0,0 +1,12 @@
+import LoginForm from "../components/loginForm/loginForm";
+import Layout from "../layout/layout";
+
+const LoginPage = () => {
+  return (
+    <Layout>
+      <LoginForm />
+    </Layout>
+  );
+};
+
+export default LoginPage;
