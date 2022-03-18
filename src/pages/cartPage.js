1e8f0d26c3147db07cefb5e554534037418f9954
esmailireza <esmailireza20@gmail.com>
2022-03-18 10:57:57 +0330

    redesign cart page

--- src/pages/cartPage.js
+++ src/pages/cartPage.js
@@ -3,6 +3,8 @@ import { useCart, useCartActions } from "../providers/cartProvider";
 import "./cartPage.css";
 import { BiTrash } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import { MdAddShoppingCart } from "react-icons/md";
+import Button from "@mui/material/Button";
 
 const CartPage = () => {
   const cartState = useCart();
@@ -18,65 +20,81 @@ const CartPage = () => {
   };
   return (
     <Layout>
-      <main className="container d-flex justify-content-between mt-5">
-        <section className="col-sm-8">
-          {cartState.cart.length ? (
-            cartState.cart.map((item) => {
-              return (
-                <div key={item.id} className="cartItem mt-4">
-                  <div className="cartItemDesAndImg">
-                    <img
-                      src={item.image}
-                      className="cartItemImage"
-                      alt={item.name}
-                    />
-                    <div className="cartItemDescription">
-                      <h2>{item.name}</h2>
-                      <p>{item.description}</p>
-                      <div>$ {item.price * item.quantity}</div>
+      <main className="container marginMainCart">
+        {cartState.cart.length ? (
+          cartState.cart.map((item) => {
+            return (
+              <div className=" col-sm-12 d-flex justify-content-between mt-5">
+                <section className="col-sm-8">
+                  <div key={item.id} className="cartItem mt-4">
+                    <div className="cartItemDesAndImg">
+                      <img
+                        src={item.image}
+                        className="cartItemImage"
+                        alt={item.name}
+                      />
+                      <div className="cartItemDescription">
+                        <h2>{item.name}</h2>
+                        <p>{item.description}</p>
+                        <div>$ {item.price * item.quantity}</div>
+                      </div>
+                    </div>
+                    <div>
+                      <button
+                        onClick={() => onDecrement(item)}
+                        className={`button ${
+                          item.quantity === 1 && "trashBtn"
+                        }`}
+                      >
+                        {item.quantity > 1 ? "-" : <BiTrash />}
+                      </button>
+                      <span /* className={styles.value} */>
+                        {item.quantity}
+                      </span>
+
+                      <button
+                        onClick={() => onIncrement(item)}
+                        className="button increment"
+                      >
+                        +
+                      </button>
                     </div>
                   </div>
-                  <div>
-                    <button
-                      onClick={() => onDecrement(item)}
-                      className={`button ${item.quantity === 1 && "trashBtn"}`}
+                </section>
+                <section className="col-sm-4 py-4 h-50 bg-light">
+                  <h2>price total</h2>
+                  <p>subTotal: $ {originalTotalPrice}</p>
+                  <p>
+                    Product Discounts:
+                    <span className="text-danger">
+                      {" "}
+                      $ {Math.round(originalTotalPrice - cartState.total)}
+                    </span>{" "}
+                  </p>
+                  <h4>Total: $ {cartState.total}</h4>
+                  <button type="button" class="btn btn-primary w-100 mt-4">
+                    <Link
+                      to="/checkout"
+                      className="text-white text-decoration-none"
                     >
-                      {item.quantity > 1 ? "-" : <BiTrash />}
-                    </button>
-                    <span /* className={styles.value} */>{item.quantity}</span>
-
-                    <button
-                      onClick={() => onIncrement(item)}
-                      className="button increment"
-                    >
-                      +
-                    </button>
-                  </div>
-                </div>
-              );
-            })
-          ) : (
-            <p>no item in cart!</p>
-          )}
-        </section>
-
-        <section className="col-sm-4 py-4 h-50 bg-light">
-          <h2>price total</h2>
-          <p>subTotal: $ {originalTotalPrice}</p>
-          <p>
-            Product Discounts:
-            <span className="text-danger">
-              {" "}
-              $ {Math.round(originalTotalPrice - cartState.total)}
-            </span>{" "}
-          </p>
-          <h4>Total: $ {cartState.total}</h4>
-          <button type="button" class="btn btn-primary w-100 mt-4">
-            <Link to="/checkout" className="text-white text-decoration-none">
-              Go to checkout
+                      Go to checkout
+                    </Link>
+                  </button>
+                </section>
+              </div>
+            );
+          })
+        ) : (
+          <div>
+            <MdAddShoppingCart className="cartEmptyIcon mt-4" />
+            <h4 className="mt-4">Cart is empty</h4>
+            <Link to="/shop" className="text-decoration-none">
+              <Button variant="outlined" className="my-5">
+                Go to products
+              </Button>
             </Link>
-          </button>
-        </section>
+          </div>
+        )}
       </main>
     </Layout>
   );
