a27a08efd6411a7c003e161839ea703c200b3f8f
esmailireza <esmailireza20@gmail.com>
2022-03-29 21:15:20 +0430

    add logo image to login page

--- src/components/loginForm/loginForm.js
+++ src/components/loginForm/loginForm.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import * as Yup from "yup";
 import Input from "../../common/input";
 import styles from "./login.module.css";
+import logo from "../../assets/images/amazon-logo.png";
 const LoginForm = () => {
   const onSubmit = (values) => {
     console.log(values);
@@ -28,23 +29,38 @@ const LoginForm = () => {
     onSubmit,
   });
   return (
-    <form className={styles.formContainer} onSubmit={formik.handleSubmit}>
-      <Input label="email" name="email" formik={formik} type="email" />
-      <Input label="password" name="password" formik={formik} type="password" />
-      <button
-        type="submit"
-        className={styles.button}
-        disabled={!formik.isValid}
-      >
-        Login
-      </button>
-      <Link
-        to="/signupPage"
-        className={`text-start mt-2 text-decoration-none ${styles.headToSignup}`}
-      >
-        <p className="text-decoration-none">Do not have an account? Sign Up</p>
+    <main className="d-flex flex-column">
+      <Link to="/">
+        <img src={logo} className={styles.logoLogin} />
       </Link>
-    </form>
+      <form className={styles.formContainer} onSubmit={formik.handleSubmit}>
+        <div className={styles.textAlign}>
+          <h3>Sign-In</h3>
+        </div>
+        <Input label="email" name="email" formik={formik} type="email" />
+        <Input
+          label="password"
+          name="password"
+          formik={formik}
+          type="password"
+        />
+        <button
+          type="submit"
+          className={styles.button}
+          disabled={!formik.isValid}
+        >
+          Login
+        </button>
+        <Link
+          to="/signupPage"
+          className={`text-start mt-2 text-decoration-none ${styles.headToSignup}`}
+        >
+          <p className="text-decoration-none">
+            Do not have an account? Sign Up
+          </p>
+        </Link>
+      </form>
+    </main>
   );
 };
 
