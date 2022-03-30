285883de1d1113dca573dda3218a6b835be5fc0c
esmailireza <esmailireza20@gmail.com>
2022-03-30 19:00:07 +0430

    create checkout page component

---
+++ src/pages/checkout/checkoutPage.js
@@ -0,0 +1,11 @@
+import Layout from "../../layout/layout";
+
+const CheckoutPage = () => {
+  return (
+    <Layout>
+      <h3>checkout page</h3>
+    </Layout>
+  );
+};
+
+export default CheckoutPage;
