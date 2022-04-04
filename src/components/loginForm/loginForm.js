92480afe13a7b53bf677fb94581ee6e5db9be16c
esmailireza <esmailireza20@gmail.com>
2022-04-05 02:54:32 +0430

    fix login page responsive

--- src/components/loginForm/loginForm.js
+++ src/components/loginForm/loginForm.js
@@ -38,24 +38,26 @@ const LoginForm = () => {
     onSubmit,
   });
   return (
-    <main className="d-flex flex-column">
-      <Link to="/">
-        <img src={logo} className={styles.logoLogin} alt="amazon" />
-      </Link>
-      <form className={styles.formContainer} onSubmit={formik.handleSubmit}>
-        <div className={styles.textAlign}>
-          <h3>Sign-In</h3>
-        </div>
-        <Input label="email" name="email" formik={formik} type="email" />
-        <Input
-          label="password"
-          name="password"
-          formik={formik}
-          /* type="password" */
-          style={{ position: "relative" }}
-          type={showPassword ? "text" : "password"}
-        />
-        {/* <MdVisibilityOff
+    <div className="container">
+      <main className="col-sm-12 d-flex justify-content-center align-items-center">
+        <div className="col-sm-5 d-flex flex-column">
+          <Link to="/">
+            <img src={logo} className={styles.logoLogin} alt="amazon" />
+          </Link>
+          <form className={styles.formContainer} onSubmit={formik.handleSubmit}>
+            <div className={styles.textAlign}>
+              <h3>Sign-In</h3>
+            </div>
+            <Input label="email" name="email" formik={formik} type="email" />
+            <Input
+              label="password"
+              name="password"
+              formik={formik}
+              /* type="password" */
+              style={{ position: "relative" }}
+              type={showPassword ? "text" : "password"}
+            />
+            {/* <MdVisibilityOff
           style={{
             fontSize: "1.6rem",
             position: "absolute",
@@ -63,23 +65,25 @@ const LoginForm = () => {
             left: "60rem", 
           }}
         /> */}
-        <button
-          type="submit"
-          className={styles.button}
-          disabled={!formik.isValid}
-        >
-          Login
-        </button>
-        <Link
-          to="/signupPage"
-          className={`text-start mt-2 text-decoration-none ${styles.headToSignup}`}
-        >
-          <p className="text-decoration-none">
-            Do not have an account? Sign Up
-          </p>
-        </Link>
-      </form>
-    </main>
+            <button
+              type="submit"
+              className={styles.button}
+              disabled={!formik.isValid}
+            >
+              Login
+            </button>
+            <Link
+              to="/signupPage"
+              className={`text-start mt-2 text-decoration-none ${styles.headToSignup}`}
+            >
+              <p className="text-decoration-none">
+                Do not have an account? Sign Up
+              </p>
+            </Link>
+          </form>
+        </div>
+      </main>
+    </div>
   );
 };
 
