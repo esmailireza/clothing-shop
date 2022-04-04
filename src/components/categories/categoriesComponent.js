cdb8fc1f04a5201fa3e32818d7decc180a8629e6
esmailireza <esmailireza20@gmail.com>
2022-04-04 13:33:39 +0430

    fix categories responsive

--- src/components/categories/categoriesComponent.js
+++ src/components/categories/categoriesComponent.js
@@ -6,7 +6,11 @@ import styles from "./categories.module.css";
 const Categories = () => {
   return (
     <div className={`my-5 ${styles.customeContainer}`}>
-      <div class="row row-cols-1 row-cols-md-3">
+      {/*       <div class="row row-cols-1 row-cols-md-3">
+       */}
+      <div
+        className={`row row-cols-1 row-cols-md-3 ${styles.responsiveCategories}`}
+      >
         <div class="col mb-4">
           <Link to="/shop" className="text-decoration-none text-reset">
             <div className={`card ${styles.categoryHover}`}>
@@ -17,6 +21,8 @@ const Categories = () => {
               </div>
             </div>
           </Link>
+          {/*         </div>
+           */}
         </div>
         <div class="col mb-4">
           <Link to="/shop" className="text-decoration-none text-reset">
