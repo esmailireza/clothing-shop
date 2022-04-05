99706f6b4501fae3e7bc8f9ce9f9165fb7f97117
esmailireza <esmailireza20@gmail.com>
2022-04-05 12:57:20 +0430

    fix shop page responsive

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -18,46 +18,40 @@ const Shop = () => {
   };
   return (
     <Layout>
-      <div class="container" className={styles.customeContainer}>
-        <div className="row">
-          <div className="d-flex flex-wrap col-sm-12">
-            {data.products.map((product) => {
-              /* console.log(data); */
-              return (
-                <Link
-                  to={`/products/${product.name}`}
-                  product={product}
-                  className={`card text-decoration-none text-dark ${styles.cardLayout}`}
-                  key={product.id}
-                >
-                  <img
-                    src={product.image}
-                    alt={product.name}
-                    className={`card-img-top ${styles.cardImage}`}
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">{product.name}</h5>
-                    <p className="card-text">{product.description}</p>
-                    <div className="d-flex justify-content-between align-items-center">
-                      <span className="font-weight-bold">
-                        $ {product.price}
-                      </span>
-                      <button
-                        onClick={() => addProductHandler(product)}
-                        href="#"
-                        className="btn btn-primary"
-                      >
-                        {CheckInCart(cartCheck.cart, product)
-                          ? "In cart"
-                          : "Add To Cart"}
-                      </button>
-                    </div>
-                  </div>
-                </Link>
-              );
-            })}
-          </div>
-        </div>
+      <div className={styles.customeContainer}>
+        {data.products.map((product) => {
+          /* console.log(data); */
+          return (
+            <Link
+              to={`/products/${product.name}`}
+              product={product}
+              className={`card text-decoration-none text-dark ${styles.cardLayout}`}
+              key={product.id}
+            >
+              <img
+                src={product.image}
+                alt={product.name}
+                className={`card-img-top ${styles.cardImage}`}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{product.name}</h5>
+                <p className="card-text">{product.description}</p>
+                <div className="d-flex justify-content-between align-items-center">
+                  <span className="font-weight-bold">$ {product.price}</span>
+                  <button
+                    onClick={() => addProductHandler(product)}
+                    href="#"
+                    className="btn btn-primary"
+                  >
+                    {CheckInCart(cartCheck.cart, product)
+                      ? "In cart"
+                      : "Add To Cart"}
+                  </button>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </Layout>
   );
